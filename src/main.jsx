42484d668ec9c604e4d4f1cbca6847f63c319fe5
Angela Li <angela.tylee25@gmail.com>
2025-01-20 21:04:29 +0800

    Replace `BrowserRouter` with `HashRouter`, remove `basename` from `main.jsx`

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App.jsx'
 // import './index.css'
 import './assets/scss/all.scss'
@@ -9,7 +9,7 @@ import { ThemeProvider } from './context/ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router basename="/portfolio/">
+    <Router>
       <ThemeProvider>
         <App />
       </ThemeProvider>
